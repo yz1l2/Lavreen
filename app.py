dd0b688c31317831771c93ae3ea882fe018cc6c1
Naif <2qqq9111@gmail.com>
2026-08-18 01:27:22 +0300

    Update sell page with responsive mobile layout and all categories

--- app.py
+++ app.py
@@ -86,4 +86,5 @@ def my_listings():
     return render_template('my_listings.html', listings=listings)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
